2c5a918336636e6d11bd3f7ebf03f4d0f7b52817
eloca <eloca@192.168.1.141>
2020-07-31 14:01:23 +0200

    version 31 de julio

---
+++ WebContent/registro.js
@@ -0,0 +1,71 @@
+//EL NOMBRE DEL USUARIO TIENE QUE SER MINIMO DE 2
+//LA PWD DEL USUARIO TIENE QUE SER DE AL MENOS 2
+
+//TODO validar formulario 
+function nombreCorrecto (nombre)
+{
+	let correcto = false;
+		
+		correcto = (nombre.length >=2);
+	
+	return correcto;
+}
+
+function pwdCorrecto (pwd)
+{
+	let correcto = false;
+		
+		correcto = (pwd.length >=2);
+	
+	return correcto;
+}
+
+function activarBotonEnvio()
+{
+	let boton = document.getElementById("botonalta");
+	boton.disabled = false;
+}
+
+function desactivarBotonEnvio()
+{
+	let boton = document.getElementById("botonalta");
+	boton.disabled = true;
+}
+
+function modificadoFormulario() 
+{
+	let nuevo_nombre = document.getElementById("nombre").value;
+	let nuevo_pwd = document.getElementById("pwd").value;
+	
+	if (nombreCorrecto(nuevo_nombre)&&(pwdCorrecto(nuevo_pwd)))
+		{
+			activarBotonEnvio();
+		} else {
+			desactivarBotonEnvio();
+		}
+	
+	//mostrando errores personalizados
+	/*let nombre_correcto =  false;
+	let pwd_correcto =  false;
+	
+	if (nombreCorrecto(nuevo_nombre))
+		{
+			nombre_correcto = true;
+		} else {
+			mostrarErrorNombre();
+		}
+	if (pwdCorrecto (nuevo_pwd))
+	{
+		pwd_correcto = true;
+	} else {
+		mostrarErrorPwd();
+	}
+	 
+	if (nombre_correcto&&ppwd_correcto)
+		{
+		activarBotonEnvio();
+		} else {
+			desactivarBotonEnvio();
+		}
+	*/
+}
