e1057626f79621301ae39dbabb896004d09d0c8c
eloca <eloca@192.168.1.141>
2020-07-31 12:38:52 +0200

    version 31 de julio

--- src/loginjee/persistencia/UsuarioDAO.java
+++ src/loginjee/persistencia/UsuarioDAO.java
@@ -3,6 +3,8 @@ package loginjee.persistencia;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -12,19 +14,100 @@ import loginjee.bean.Usuario;
 public class UsuarioDAO {
 	
 	//DAO Data ACCESS OBJEECT
-	//ESTA CLASE, SER√Å INVOCADA POR EL SERVICIO
+	//ESTA CLASE, SER¡ INVOCADA POR EL SERVICIO
 	
 	private final static Logger log = Logger.getLogger("mylog");
 	private static final String INSTRUCCION_CONSULTA_USUARIO = "SELECT * FROM hedima.usuarios WHERE (nombre = ?);";
+	private static final String INSTRUCCION_CONSULTA_TODOS_USUARIO = "SELECT * FROM hedima.usuarios;";
+	private static final String INSTRUCCION_CONSULTA_USUARIO_POR_ID = "SELECT * from hedima.usuarios where idusuarios = ?;";
+	private static final String INSTRUCCION_INSERCION_USUARIO = "INSERT INTO hedima.usuarios (nombre, password) VALUES (?, ?);";
 	
-	public List<Usuario> obtenerTodos ()
+	
+	public Usuario leerUsuarioBD (int id) throws Exception
 	{
-		//AQU√ç METEMOS LO DEL API JDBC : CONNECTION, RESULTSET, ETC.
-		return null;
+		Usuario usuario = null;
+		Connection connection = null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try{
+			String consulta_SQL = INSTRUCCION_CONSULTA_USUARIO_POR_ID;
+			connection = BaseDeDatos.getConnection();
+			st = connection.prepareStatement(consulta_SQL);
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			
+			if (rs.next())
+			{
+				usuario = new Usuario(rs);
+			}
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+			throw e;
+		} finally{
+			BaseDeDatos.liberarRecursos(connection, st, rs);
+		}
+		return usuario;
+	}
+	
+	public void altaUsuarioBD (Usuario usuario) throws Exception
+	{
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			connection = BaseDeDatos.getConnection();
+			preparedStatement = connection.prepareStatement(INSTRUCCION_INSERCION_USUARIO);
+			preparedStatement.setString(1, usuario.getNombre());
+			preparedStatement.setString(2, usuario.getPwd());
+			int resultado = preparedStatement.executeUpdate();// siempre executeUpdate para INSERTAR; DELETE; o UPDATE para SELECT executeQuery()
+			log.debug("resultado insertar = " + resultado);
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+			log.error("Error en la insercion de usuario " + usuario , e);
+			throw e;
+		}finally {
+			BaseDeDatos.liberarRecursos(connection, preparedStatement, null);
+		}
+	}
+	
+	public List<Usuario> obtenerTodos () throws Exception
+	{
+		List<Usuario> lista_usuarios = null;
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		Usuario usuario_aux = null;
+		
+		
+			try {
+				
+				connection = BaseDeDatos.getConnection();
+				statement = connection.createStatement();
+				resultSet = statement.executeQuery(INSTRUCCION_CONSULTA_TODOS_USUARIO);
+				
+				lista_usuarios = new ArrayList<Usuario>();//creo la lista vacÌa
+				while (resultSet.next()) {
+					usuario_aux = new Usuario(resultSet);
+					lista_usuarios.add(usuario_aux);
+				}
+				
+			}catch (Exception e) {
+				log.error("Error al acceder a la base de datos", e);
+				throw e;
+			}finally {
+				BaseDeDatos.liberarRecursos(connection, statement, resultSet);
+			
+			}
+		
+		return lista_usuarios;
+		
 	}
 	
 	/**
-	 * M√©todo que accede a la base de datos y comprueba si el usuario recibido existe
+	 * MÈtodo que accede a la base de datos y comprueba si el usuario recibido existe
 	 * @param usuario el usuario que queremos comprobar
 	 * @return 0 si existe 1 si falla el pwd (pero existe el nombre) 2 no existe (no coincide ni nombre ni pwd)
 	 * @throws Exception si hubo fallo con la base de datos
@@ -50,11 +133,11 @@ public class UsuarioDAO {
 				System.out.println("USUARIO RECUPERADO DE LA BD " + usuario2);
 				if (usuario2.getPwd().equals(usuario.getPwd()))
 				{
-					log.debug ("hay un usuario que coincide por el nombre y por la contrase√±a");
+					log.debug ("hay un usuario que coincide por el nombre y por la contraseÒa");
 					num_dev = 0;
 				} else 
 				{
-					log.debug ("hay un usuario que coincide por el nombre pero NO por la contrase√±a");
+					log.debug ("hay un usuario que coincide por el nombre pero NO por la contraseÒa");
 					num_dev = 1;
 				}
 				
--- src/loginjee/servicio/UsuarioService.java
+++ src/loginjee/servicio/UsuarioService.java
@@ -8,13 +8,24 @@ import loginjee.bean.Usuario;
 import loginjee.persistencia.UsuarioDAO;
 
 public class UsuarioService {
-	//TENDEREMOS QUE TENER TODOS LOS MÃ‰TODOS RELATIVOS AL TRABAJO CON USUARIOS
-	//ESTA CLASE SERÃ� INVOCADA POR EL SERVLET
-	//Y ESTA CLASE, INVOCARÃ� A LA PERSISTENCIA (EN CASO DE SER NECESARIO)
+	//TENDEREMOS QUE TENER TODOS LOS MÉTODOS RELATIVOS AL TRABAJO CON USUARIOS
+	//ESTA CLASE SERÁ INVOCADA POR EL SERVLET
+	//Y ESTA CLASE, INVOCARÁ A LA PERSISTENCIA (EN CASO DE SER NECESARIO)
 	
 	private final static Logger log = Logger.getLogger("mylog");
 	
-	public List<Usuario> listarUsuarios ()
+	
+	public Usuario obtenerUsuario(int id_usuario) throws Exception
+	{
+		Usuario usuario = null;
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		
+			usuario = usuarioDAO.leerUsuarioBD(id_usuario);
+		
+		return usuario;
+	}
+	
+	public List<Usuario> listarUsuarios () throws Exception
 	{
 		List<Usuario> lUsuarios = null;
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
@@ -26,7 +37,7 @@ public class UsuarioService {
 	
 	
 	/**
-	 * MÃ©todo que comprueba si el usuario recibido existe en el sistema
+	 * Método que comprueba si el usuario recibido existe en el sistema
 	 * @param usuario el usuario que queremos comprobar
 	 * @return 0 si existe 1 si falla el pwd (pero existe el nombre) 2 no existe (no coincide ni nombre ni pwd)
 	 * @throws Exception si hubo fallo con la base de datos
@@ -45,6 +56,17 @@ public class UsuarioService {
 			log.debug("Salimos en servicio existeUsuario valor devuelto = " + num_dev);
 		
 		return num_dev;
+		
+	}
+	
+	public void altaUsuario (Usuario usuario) throws Exception
+	{
+		UsuarioDAO usuarioDAO = null;
+		
+			usuarioDAO = new UsuarioDAO();
+			usuarioDAO.altaUsuarioBD(usuario);
+			log.debug("Salimos del servicio altaUsuario ");
+			
 		
 	}
 
